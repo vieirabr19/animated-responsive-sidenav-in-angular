abed11cb65cfea8a0f7de87c1cb3dbbcb772f3a9
Luiz Vieira <luiz22_sp@hotmail.com>
2023-11-09 22:28:40 -0300

    Projeto navbar em angular - parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'animated-responsive-sidenav-in-angular';
+  collapsed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle) {
+    this.screenWidth = data.screenWidth;
+    this.collapsed = data.collapsed;
+  }
 }
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,9 @@
+export const navbarData = [
+  { routerLink: 'dashboard', icon: 'fa-home', label: 'Dashboard' },
+  { routerLink: 'products', icon: 'fa-box-open', label: 'Products' },
+  { routerLink: 'statistics', icon: 'fa-chart-bar', label: 'Statistics' },
+  { routerLink: 'coupens', icon: 'fa-tags', label: 'Coupens' },
+  { routerLink: 'pages', icon: 'fa-file', label: 'Pages' },
+  { routerLink: 'media', icon: 'fa-camera', label: 'Media' },
+  { routerLink: 'settings', icon: 'fa-cog', label: 'Settings' },
+];
